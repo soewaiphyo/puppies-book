fe60ca5bec00bce1cf76764d8d532372fcc05e00
Phyo <soewaiphyo.jelly@gmail.com>
2020-10-22 16:47:23 +0800

    Updates

---
+++ precache-manifest.e4f78ccd0b5f543603bbad8940231cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7970834db8075b4fab63855af3030c",
+    "url": "/puppies-book/index.html"
+  },
+  {
+    "revision": "8310d6b18184f17509ba",
+    "url": "/puppies-book/static/css/main.82ee4a33.chunk.css"
+  },
+  {
+    "revision": "90f6f8dd061a6b19446e",
+    "url": "/puppies-book/static/js/2.214dfab0.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/puppies-book/static/js/2.214dfab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8310d6b18184f17509ba",
+    "url": "/puppies-book/static/js/main.be0b4d6f.chunk.js"
+  },
+  {
+    "revision": "9a0099496dc9e6d5e0d4",
+    "url": "/puppies-book/static/js/runtime-main.67117b0a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/puppies-book/static/media/logo.ee7cd8ed.svg"
+  }
+]);
